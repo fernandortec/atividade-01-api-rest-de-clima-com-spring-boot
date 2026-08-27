8ef4614e934eaf627692b53f4ab21df186ecb8e1
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2026-08-27 11:05:00 -0300

    config: initializing project

---
+++ src/test/java/api_clima/atividade_01/Atividade01ApplicationTests.java
@@ -0,0 +1,13 @@
+package api_clima.atividade_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
